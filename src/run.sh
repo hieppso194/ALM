70f30b5e6765266aed6ec4834595ed19adb9561e
Henry PHAM <hieppso194@gmail.com>
2018-02-15 11:04:29 +0100

    edit run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 case $1 in
 	repo)
-		echo "Source code is available in: https://github.com/hieppso194/ALM.git"
+		echo "Source code and data are available in: https://github.com/hieppso194/ALM.git"
 		;;
 	help)
 		echo "To use the first approach: ./run.sh 1"
 		echo "To use the second approach/; ./run.sh 2"
-		echo "Hint: the first use Python2, the second one use Python3"
+		echo "Hint: The first approach uses Python2, the second one uses Python3"
 		;;
 	1)
 		python lr.py
