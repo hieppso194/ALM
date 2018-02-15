5c9b84685d8c720aed729a61b6d2b7e79f1e20de
Henry PHAM <hieppso194@gmail.com>
2018-02-15 10:49:34 +0100

    Edit code and initialize run script

--- src/hmm.py
+++ src/hmm.py
@@ -1,9 +1,5 @@
 import numpy as np
 import csv
-from collections import Counter
-from sklearn.model_selection import train_test_split
-import pandas as pd
-
 
 DNA_characters = ['A', 'C', 'G', 'T']
 def extend_bag_words(bag_words, n_gram=4):
@@ -19,8 +15,6 @@ five_gram_bag_words = extend_bag_words(four_gram_bag_words)
 six_gram_bag_words = extend_bag_words(five_gram_bag_words)
 seven_gram_bag_words = extend_bag_words(six_gram_bag_words)
 bag_words = [0,DNA_characters,two_gram_bag_words,three_gram_bag_words,four_gram_bag_words,five_gram_bag_words,six_gram_bag_words,seven_gram_bag_words]
-# print (bag_words)
-
 
 def estimate_prob(t_0,t_1,f_0,f_1,f,t):
     for i in range(0,len(f)):
@@ -28,12 +22,10 @@ def estimate_prob(t_0,t_1,f_0,f_1,f,t):
         f_1[i] = float((f_1[i]+1)/(f[i]+1))
     for i in range(0,t.shape[0]):
         for j in range(0,t.shape[1]):
-            t_0[i][j] = float((t_0[i][j]+1)/(t[i][j]+1))
-            t_1[i][j] = float((t_1[i][j]+1)/(t[i][j]+1))
+            t_0[i][j] = float((t_0[i][j]+1)/((t[i][j]+1)*(f_0[i])))
+            t_1[i][j] = float((t_1[i][j]+1)/(t[i][j]+1)*(f_1[i]))
     return t_0,t_1,f_0,f_1
 
-
-# t_i - transition matrix|y=i, f_0 - count(word|y=0), f_1 - count(word|y=1), f_i = count(word_i) t - count transition matrix
 def estimate(row,t_0,t_1,f_0,f_1,f,t,n_gram,y):
     bag_word = bag_words[n_gram]
     word_pre = '0'
@@ -123,8 +115,6 @@ def hmm(filename,y,n_gram):
             # encode_row = n_gram_encode(row,n_gram)
     t_0, t_1, f_0, f_1 = estimate_prob(t_0,t_1,f_0,f_1,f,t)
     return X,t_0, t_1, f_0, f_1
-
-
 # ============================================================================================
 def read_label(filename):
     label = list()
@@ -136,7 +126,6 @@ def read_label(filename):
     label = np.asarray(label)
     return label
 
-# ------------------------------------------------------------------------------------------
 def read_data(filename):
     data = []
     with open(filename) as csvfile:
@@ -148,10 +137,7 @@ def read_data(filename):
             data.append(row)
     return data
 
-# ------------------------------------------------------------------------------------------
-
-
-
+# ==================================================================================================
 def accuracy_score(y_pred,y_label):
     count = 0
     N = len(y_pred)
@@ -161,88 +147,38 @@ def accuracy_score(y_pred,y_label):
 
     score = float(count) / N
     return score
-
-# Perceptron
-
-
-# print (count_zero_value([0,0,1],0.8))
-
-# ======================================================================================================================
-# Test data
 # =============================================================
-# Train dataset===========================================================
-# print(n_gram_encode("TAGATGTCAATTTTAAACTAAGGCTTGATATACTTGGCCTTCAGGTAAAGAGCGGCTATCCCAGTCAAATGTGACTGGCACCCAAGCAGATTATAAGGGAG",2))
 n_gram = 6
+print ("Experiment with the second approach with n_grams : ",n_gram)
+X = read_data('../data/Xtr0.csv')
+X1 = read_data('../data/Xtr1.csv')
+X2 = read_data('../data/Xtr2.csv')
+
 Y = read_label('../data/Ytr0.csv')
 Y1 = read_label('../data/Ytr1.csv')
 Y2 = read_label('../data/Ytr2.csv')
 
-Y_all = np.concatenate((Y,Y1,Y2),axis=0)
-X,t_0,t_1,f_0,f_1 = hmm('../data/Xtr0.csv',Y,n_gram)
-X1,t_0_1,t_1_1,f_0_1,f_1_1 = hmm('../data/Xtr1.csv',Y1,n_gram)
-X2,t_0_2,t_1_2,f_0_2,f_1_2 = hmm('../data/Xtr2.csv',Y2,n_gram)
-Xt = read_data('../data/Xtr0.csv')
-Xt1 = read_data('../data/Xtr1.csv')
-Xt2 = read_data('../data/Xtr2.csv')
-# X_all = np.concatenate((X,X1,X2),axis=0)
-# t_0_all,t_1_all,f_0_all,f_1_all = hmm_train(X_all,Y_all,n_gram)
-# print (X_all.shape)
-#
-# Xt = hmm('../data/Xte0.csv',n_gram,Y)
-# Xt1 = hmm('../data/Xte1.csv',n_gram,Y)
-# Xt2 = hmm('../data/Xte2.csv',n_gram,Y)
+Xt = read_data('../data/Xte0.csv')
+Xt1 = read_data('../data/Xte1.csv')
+Xt2 = read_data('../data/Xte2.csv')
+# ============================================================================================================================
 
-
-# ================================================================================================
-# label train dataset
-
-#
-# --------------------------------------------------------------------
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33)
-# X_train = np.transpose(X_train)
-# X_test = np.transpose(X_test)
-#
-X_train1, X_test1, y_train1, y_test1 = train_test_split(X1, Y1, test_size=0.33)
-# X_train1 = np.transpose(X_train1)
-# X_test1 = np.transpose(X_test1)
-#
-X_train2, X_test2, y_train2, y_test2 = train_test_split(X2, Y2, test_size=0.33)
-# X_train2 = np.transpose(X_train2)
-# X_test2 = np.transpose(X_test2)
-# all dataset
-# X_train_all, X_test_all, y_train_all, y_test_all = train_test_split(X_all.T, Y_all, test_size=0.33)
-# X_train_all = np.transpose(X_train_all)
-# X_test_all = np.transpose(X_test_all)
-
-# y_train_all = np.transpose(y_train_all)
-
-# # ====================================================================
-#  initialize parameters
-# eta = .005
-# d = X_train_all.shape[0]
-# w_init = np.random.rand(d,1)
-#===============================================================================
-# w1 = logistic_regression(X1, Y1, w_init, eta)
-# wtest_all = logistic_regression(X_train_all,y_train_all,w_init,eta)
-
-# #==============================================================================
-# w = logistic_regression(X, Y, w_init, eta)
-# w1 = logistic_regression(X1, Y1, w_init, eta)
-# w2 = logistic_regression(X2, Y2, w_init, eta)
-# wtest_all = w
-# y_pred_test_all = hmm_predict(X_test_all,wtest_all)
-# w_all = logistic_regression(X_all,Y_all,w_init,eta)
-# y_pred_all = hmm_predict(X_all,n_gram,t_0_all,t_1_all,f_0_all,f_1_all)
-# y_pred = hmm_predict(Xt,n_gram,t_0,t_1,f_0,f_1)
-# y_pred1 = hmm_predict(Xt1,n_gram,t_0_1,t_1_1,f_0_1,f_1_1)
-# y_pred2 = hmm_predict(Xt2,n_gram,t_0_2,t_1_2,f_0_2,f_1_2)
+print ("Training Xtr0...")
+t_0,t_1,f_0,f_1 = hmm_train(X,Y,n_gram)
+print ("Train Xtr0 completed")
+print ("Training Xtr1...")
+t_0_1,t_1_1,f_0_1,f_1_1 = hmm_train(X1,Y1,n_gram)
+print ("Train Xtr1 completed")
+print("Training Xtr2")
+t_0_2,t_1_2,f_0_2,f_1_2 = hmm_train(X2,Y2,n_gram)
+print ("Train Xtr2 completed")
 #============================================================================================================================
-# yte = predict(Xt,w)
-# yte1 = predict(Xt1,w1)
-# yte2 = predict(Xt2,w2)
+print ("Predicting the test datasets")
 yte = hmm_predict(Xt,n_gram,t_0,t_1,f_0,f_1)
 yte1 = hmm_predict(Xt1,n_gram,t_0_1,t_1_1,f_0_1,f_1_1)
 yte2 = hmm_predict(Xt2,n_gram,t_0_2,t_1_2,f_0_2,f_1_2)
+
+print ("Writing the results to Yte.csv")
 thefile = open('../data/Yte.csv', 'w')
 thefile.write("%s\n" % "Id,Bound")
 for index, item in enumerate(yte):
@@ -260,13 +196,4 @@ for index, item in enumerate(yte2):
     thefile.write("%s" % ",")
     thefile.write("%s\n" % item)
 
-print ("Write completed")
-# #
-# #
-#
-# print ("Write completed")
-#
-# print (accuracy_score(y_pred_all,Y_all))
-# print (accuracy_score(y_pred,Y))
-# print (accuracy_score(y_pred1,Y1))
-# print (accuracy_score(y_pred2,Y2))
+print ("Write Completed, The result is now in data/Yte.csv.")
--- src/lr.py
+++ src/lr.py
@@ -34,60 +34,9 @@ def encode_data(filename):
             convert_data.append(encode_row)
     convert_data = np.asarray(convert_data)
     return convert_data.T
-
-# -----------------------------------------------------------------------------------------
-# encode numerical
-def transfer_number(row):
-    DNA = 'ACGT'
-    char_to_int = dict((c,i) for i, c in enumerate(DNA))
-    integer_encode = [char_to_int[char] for char in row]
-
-    return integer_encode
-
-def encode_numerical(filename):
-    convert_data = list()
-    with open(filename) as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=';')
-        for index, row in enumerate(readCSV):
-            row = str(row)
-            row = row.replace("['", "")
-            row = row.replace("']", "")
-
-            encode_row = transfer_number(row)
-            convert_data.append(encode_row)
-    convert_data = np.asarray(convert_data)
-    return convert_data.T
-
-# ========================================================================================
-# 2-3 gram encode
-def encode_gram(n_gram,row):
-    encode_squence = list()
-    for idex in range(0,len(row)-n_gram+1):
-        i = idex
-        i_gram = i+n_gram
-        sub_sequence = row[i:i_gram]
-        encode_squence.append(encode_onehot(sub_sequence))
-    encode_squence = np.asarray(encode_squence)
-    sum_element = encode_squence.shape[0]*encode_squence.shape[1]
-    encode_squence = np.reshape(encode_squence,sum_element)
-    return encode_squence
-
-# print (encode_gram(2,'ACAGT'))
-
-def gram_encode_data(filename,n_gram):
-    convert_data = list()
-    with open(filename) as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=';')
-        for index,row in enumerate(readCSV):
-            row = str(row)
-            row = row.replace("['","")
-            row = row.replace("']","")
-
-            encode_row = encode_gram(n_gram,row)
-            convert_data.append(encode_row)
-    convert_data = np.asarray(convert_data)
-    return convert_data.T
 # ===============================================================================================
+# encode n-grams
+
 DNA_characters = ['A', 'C', 'G', 'T']
 
 def extend_bag_words(bag_words, n_gram=4):
@@ -131,12 +80,9 @@ def phi_combine_gram(row):
     four_gram_phi = phi(row,4)
     five_gram_phi = phi(row,5)
 
-    a = np.concatenate((three_gram_phi,four_gram_phi), axis=0)
-    # a  = np.concatenate((four_gram_phi,five_gram_phi),axis=0)
+    a = np.concatenate((two_gram_phi,three_gram_phi,four_gram_phi), axis=0)
 
     return a
-
-# print (phi_combine_gram('AAGATGGCGCCGGGAGGGTCGAAATTAATGTCAAGGGGCCCGGGCGCCTTGTTAAGTAGATGTCATGAGGTAAATTTCAATAAAACGCCGCCAAAGGGCAA').shape)
 
 def spectrum_encode(filename, n_gram):
 
@@ -172,8 +118,7 @@ def spectrum_encode_inverse(filename, n_gram):
         m[i,:] *= idf[i]
     return m
 
-
-
+# bag of words combines n-gram bagofword
 def spectrum_encode_combine_gram(filename):
 
     convert_data = list()
@@ -190,6 +135,8 @@ def spectrum_encode_combine_gram(filename):
     return convert_data.T
 
 # ============================================================================================
+# Read label from Ytrk.csv
+
 def read_label(filename):
     label = list()
     with open(filename) as csvfile:
@@ -200,8 +147,8 @@ def read_label(filename):
     label = np.asarray(label)
     return label
 
-# ------------------------------------------------------------------------------------------
-
+# ===============================================================================================
+# Common functions
 
 def sigmoid(s):
     return 1/(1 + np.exp(-s))
@@ -210,13 +157,15 @@ def tan(s):
 
     return (np.exp(s)-np.exp(-s))/(np.exp(s)+np.exp(-s))
 
-def logistic_regression(X, Y, w_init, eta, tol = 1e-4, max_count = 1e5, lam = 0.01):
+# ================================================================================================
+# Logistic regression
+
+def logistic_regression(X, Y, w_init, eta, tol = 1e-6, max_count = 1e5, lam = 0.01):
     w = [w_init]
-    it = 0
     N = X.shape[1]
     d = X.shape[0]
     count = 0
-    check_w_after = 20
+    check_w_after = 30
     while count < max_count:
         # choose i randomly
         mix_id = np.random.permutation(N)
@@ -224,7 +173,6 @@ def logistic_regression(X, Y, w_init, eta, tol = 1e-4, max_count = 1e5, lam = 0.
             xi = X[:,i].reshape(d,1)
             yi = Y[i]
             zi = sigmoid(np.dot(w[-1].T, xi))
-            # zi = tan(np.dot(w[-1].T, xi))
             g = (yi-zi)*xi + lam*w[-1]/N
             w_new = w[-1] + eta*g
             count +=1
@@ -234,19 +182,13 @@ def logistic_regression(X, Y, w_init, eta, tol = 1e-4, max_count = 1e5, lam = 0.
                     return w
 
             w.append(w_new)
-
-    final_tol = np.linalg.norm(w[-1]-w[-check_w_after])
-    # print (final_tol)
-    # print (count)
     return w
 
 def predict(X,w):
     pred = sigmoid(np.dot(w[-1].T,X))
-    # pred = tan(np.dot(w[-1].T,X))
     y_pred = list()
     for i in range(pred.shape[1]):
         if pred[0,i] > 0.5:
-        # if pred[0, i] > 0.0:
             y_pred.append(1)
         else:
             y_pred.append(0)
@@ -254,17 +196,9 @@ def predict(X,w):
 
     return y_pred
 
-def accuracy_score(y_pred,y_label):
-    count = 0
-    N = len(y_pred)
-    for i in range(N):
-        if y_pred[i] == y_label[i]:
-            count += 1
-
-    score = float(count) / N
-    return score
-
+# ==============================================================================================================
 # Perceptron
+
 def h(w, x):
     return np.sign(np.dot(w.T, x))
 
@@ -307,37 +241,29 @@ def predict_preceptron(w,x):
     return y_pred
 # ===================================================================================================================
 # Soft Support Vector Machine
+
 def linear_kernel(x1, x2):
     return np.dot(x1, x2)
-
 
 def polynomial_kernel(x, y, p=3):
     return (1 + np.dot(x, y)) ** p
 
-# def gaussian_kernel(x, y, sigma=5.0):
-#     return np.exp(-linalg.norm(x - y) ** 2 / (2 * (sigma ** 2)))
-
 def SSVM(X,Y,C,bound=1e-7):
     N = X.shape[0]
     D = X.shape[1]
-    # K = np.dot(X,X.T)
     K = np.zeros((N, N))
     for i in range(N):
         for j in range(N):
-            # K[i, j] = linear_kernel(X[i], X[j])
-            K[i, j] = polynomial_kernel(X[i], X[j])
+            K[i, j] = linear_kernel(X[i], X[j])
+            # K[i, j] = polynomial_kernel(X[i], X[j])
 
     P = np.dot(np.dot(Y,Y.T),K)
-    print(P.shape)
-    P = matrix(P)
-    # P = matrix(V.T.dot(V))
 
-    # P = matrix(np.dot(X, X.T))
+    P = matrix(P)
     q = matrix(-np.ones((N, 1)))
     G = matrix(np.vstack((-np.eye(N), np.eye(N))))
     h = matrix(np.vstack((np.zeros((N,1)), C*np.ones((N,1)))))
     A = np.reshape((Y.T), (1,N))
-    # A = Y
     A = A.astype(float)
     A = matrix(A)
 
@@ -355,16 +281,11 @@ def SSVM(X,Y,C,bound=1e-7):
     xS = X[sv]
     yS = Y[sv]
 
-    # print (aS)
-
     w = np.zeros(D)
     for i in range(len(aS)):
         w += aS[i] * yS[i] * xS[i]
     b = yS - np.dot(xS,w)
     b = np.mean(b)
-    print (b)
-    print (w)
-
     return w,b
 
 def predict_SSVM(X,w,b):
@@ -373,177 +294,89 @@ def predict_SSVM(X,w,b):
     return np.sign(product)
 
 # =====================================================================================================================
-#  Feature selection
-def count_zero_value(X, threshold):
+#  Measures
+
+def accuracy_score(y_pred,y_label):
     count = 0
-    for i in X:
-        if i == 0:
-            count = count + 1
-    ratio = float(count)/float(len(X))
+    N = len(y_pred)
+    for i in range(N):
+        if y_pred[i] == y_label[i]:
+            count += 1
 
-    if ratio < threshold:
-        return 0
-    else :
-        return 1
-
-print (count_zero_value([0,0,1],0.8))
-def feature_selection(X,threshold):
-    D, N = X.shape
-    features_removed = list()
-    for i in range(D):
-        if count_zero_value(X[i,:],threshold) == 1:
-            features_removed.append(i)
-
-    X = np.delete(X,features_removed,0)
-    return X,features_removed
-#
-# a = np.zeros((3,2))
-# a[1,1] = 1
-# a[2,1] = 1
-# print (a.shape)
-# print (feature_selection(a, 0.9))
-
-
+    score = float(count) / N
+    return score
 # ======================================================================================================================
-# Test data
-# =============================================================
-# Train dataset===========================================================
+# Train dataset
 
 X = spectrum_encode('../data/Xtr0.csv',5)
 X1 = spectrum_encode('../data/Xtr1.csv',5)
 X2 = spectrum_encode('../data/Xtr2.csv',5)
-#
 
-# Xa,i = feature_selection(X,0.9)
-# print (i)
-
-# X = spectrum_encode_inverse('../data/Xtr0.csv',4)
-# X1 = spectrum_encode_inverse('../data/Xtr1.csv',4)
-# X2 = spectrum_encode_inverse('../data/Xtr2.csv',4)
-
-# print (X.shape)
-# print(spectrum_encode_inverse('../data/Xtr0.csv',4))
-X_all = np.concatenate((X,X1,X2),axis=1)
-# print (X_all.shape)
-
-# combine gram
-# X = spectrum_encode_combine_gram('../data/Xtr0.csv')
-# X1 = spectrum_encode_combine_gram('../data/Xtr1.csv')
-# X2 = spectrum_encode_combine_gram('../data/Xtr2.csv')
-
-# #
-# X = np.concatenate((np.ones((1, X.shape[1])), X), axis=0)
-# X1 = np.concatenate((np.ones((1, X1.shape[1])), X1), axis=0)
-# X2 = np.concatenate((np.ones((1, X2.shape[1])), X2), axis=0)
-# X_all = np.concatenate((np.ones((1, X_all.shape[1])), X_all), axis=0)
-
-# =================================================================================================
-# Test data set
-# Xt = spectrum_encode('../data/Xte0.csv',2)
-# Xt1 = spectrum_encode('../data/Xte1.csv',2)
-# Xt2 = spectrum_encode('../data/Xte2.csv',2)
-
-# print (Xt)
-#
-Xt = spectrum_encode('../data/Xte0.csv',5)
-Xt1 = spectrum_encode('../data/Xte1.csv',5)
-Xt2 = spectrum_encode('../data/Xte2.csv',5)
-
-# Xt = np.concatenate((np.ones((1, Xt.shape[1])), Xt), axis=0)
-# Xt1 = np.concatenate((np.ones((1, Xt1.shape[1])), Xt1), axis=0)
-# Xt2 = np.concatenate((np.ones((1, Xt2.shape[1])), Xt2), axis=0)
-
-# ================================================================================================
-# label train dataset
 Y = read_label('../data/Ytr0.csv')
 Y1 = read_label('../data/Ytr1.csv')
 Y2 = read_label('../data/Ytr2.csv')
 
-Y_all = np.concatenate((Y,Y1,Y2),axis=0)
-#
-# --------------------------------------------------------------------
-# Test data from train
-# X_train, X_test, y_train, y_test = train_test_split(X.T, Y, test_size=0.33)
-# X_train = np.transpose(X_train)
-# X_test = np.transpose(X_test)
-#
-# X_train1, X_test1, y_train1, y_test1 = train_test_split(X1.T, Y1, test_size=0.33)
-# X_train1 = np.transpose(X_train1)
-# X_test1 = np.transpose(X_test1)
-#
-# X_train2, X_test2, y_train2, y_test2 = train_test_split(X2.T, Y2, test_size=0.33)
-# X_train2 = np.transpose(X_train2)
-# X_test2 = np.transpose(X_test2)
+# =================================================================================================
+# Test data set
 
-# all dataset
-X_train_all, X_test_all, y_train_all, y_test_all = train_test_split(X_all.T, Y_all, test_size=0.33)
-X_train_all = np.transpose(X_train_all)
-X_test_all = np.transpose(X_test_all)
+Xt = spectrum_encode('../data/Xte0.csv',5)
+Xt1 = spectrum_encode('../data/Xte1.csv',5)
+Xt2 = spectrum_encode('../data/Xte2.csv',5)
 
-y_train_all = np.transpose(y_train_all)
-# def convert(y):
-#     for i, v in enumerate(y):
-#         if v == 0:
-#             y[i] = -1
-#     return y
-#
-# def de_convert(y):
-#     for i, v in enumerate(y):
-#         if v == -1:
-#             y[i] = 0
-#     return y
-#
-# y_train_all = convert(y_train_all)
-# w,b = SSVM(X_train_all,y_train_all,1000)
-# y_pred_SSVM = predict_SSVM(X_test_all,w,b)
-# y_pred_SSVM = de_convert(y_pred_SSVM)
-# print (y_pred_SSVM.shape,y_test_all.shape)
+# ================================================================================================
+# Test accuracy score based on the train dataset
 
-# clf = svm.SVC(kernel='linear', C=100)
-# clf.fit(X_train_all,y_train_all)
-# y_pred_SSVM = clf.fit(X_test_all)
-# print (y_pred_SSVM.shape)
+X_train, X_test, y_train, y_test = train_test_split(X.T, Y, test_size=0.33)
+X_train = np.transpose(X_train)
+X_test = np.transpose(X_test)
 
-# # ====================================================================
-#  initialize parameters
+X_train1, X_test1, y_train1, y_test1 = train_test_split(X1.T, Y1, test_size=0.33)
+X_train1 = np.transpose(X_train1)
+X_test1 = np.transpose(X_test1)
+
+X_train2, X_test2, y_train2, y_test2 = train_test_split(X2.T, Y2, test_size=0.33)
+X_train2 = np.transpose(X_train2)
+X_test2 = np.transpose(X_test2)
+
+# ===================================================================================
+#  Initialize parameters
+
 eta = .005
-d = X_train_all.shape[0]
+d = X2.shape[0]
 w_init = np.random.rand(d,1)
 # w_init = 0.1*np.ones((d,1))
-#===============================================================================
+#===================================================================================
+# Test accuracy score based on the train dataset
+
 # wtest = logistic_regression(X_train,y_train,w_init,eta)
 # y_pred_test = predict(X_test,wtest)
-#
+
 # wtest1 = logistic_regression(X_train1,y_train1,w_init,eta)
 # y_pred_test1 = predict(X_test1,wtest1)
-#
+
 # wtest2 = logistic_regression(X_train2,y_train2,w_init,eta)
 # y_pred_test2 = predict(X_test2,wtest2)
-w1 = logistic_regression(X1, Y1, w_init, eta)
-wtest_all = logistic_regression(X_train_all,y_train_all,w_init,eta)
 
-# #==============================================================================
+# ==============================================================================
+# Learn the test dataset and write to Yte.csv
+
+print ("We are using Logistic Regression.")
+print ("Learning the dataset Xtr0.csv.....")
 w = logistic_regression(X, Y, w_init, eta)
+print ("Learned Xtr0.csv")
+print ("Learning the dataset Xtr1.csv.....")
 w1 = logistic_regression(X1, Y1, w_init, eta)
+print ("Learned Xtr1.csv")
+print ("Learning the dataset Xtr2.csv.....")
 w2 = logistic_regression(X2, Y2, w_init, eta)
-# wtest_all = w
-y_pred_test_all = predict(X_test_all,wtest_all)
-w_all = logistic_regression(X_all,Y_all,w_init,eta)
-#============================================================================================================================
-# y_pred = predict(X,w)
-# y_pred1 = predict(X1,w1)
-# y_pred2 = predict(X2,w2)
-# #
+print ("Learned Xtr2.csv")
+
+print ("Predicting the test datasets ....")
 yte = predict(Xt,w)
 yte1 = predict(Xt1,w1)
 yte2 = predict(Xt2,w2)
 
-#
-# yte_all = predict(Xt,w_all)
-# yte1_all = predict(Xt1,w_all)
-# yte2_all = predict(Xt2,w_all)
-
-# #
+print ("Writing the results to Yte.csv")
 thefile = open('../data/Yte.csv', 'w')
 thefile.write("%s\n" % "Id,Bound")
 for index, item in enumerate(yte):
@@ -560,38 +393,6 @@ for index, item in enumerate(yte2):
     thefile.write("%s" % index)
     thefile.write("%s" % ",")
     thefile.write("%s\n" % item)
-#
-# write to Yte when combine all datasets to X_all
 
-# thefile = open('../data/Yte.csv', 'w')
-# thefile.write("%s\n" % "Id,Bound")
-# for index, item in enumerate(yte_all):
-#     thefile.write("%s" % index)
-#     thefile.write("%s" % ",")
-#     thefile.write("%s\n" % item)
-# for index, item in enumerate(yte1_all):
-#     index +=1000
-#     thefile.write("%s" % index)
-#     thefile.write("%s" % ",")
-#     thefile.write("%s\n" % item)
-# for index, item in enumerate(yte2_all):
-#     index+=2000
-#     thefile.write("%s" % index)
-#     thefile.write("%s" % ",")
-#     thefile.write("%s\n" % item)
-
-print ("Write completed")
-# # # print (encode_onehot('ACAG'))
-# # #
-# # # # Perceptron test
-# # # # w = perceptron(X,Y,w_init)
-# # # # y_predict = predict_preceptron(w,X)
-# # # # # print (w[-1])
-# # # # # print (predict_preceptron(w,X))
-# # # #
-# # # # print (accuracy_score(y_predict,Y))
-# print (accuracy_score(y_pred,Y),accuracy_score(y_pred1,Y1),accuracy_score(y_pred2,Y2))
-#
-# print (accuracy_score(y_pred_test, y_test), accuracy_score(y_pred_test1, y_test1), accuracy_score(y_pred_test2, y_test2))
-print (accuracy_score(y_pred_test_all,y_test_all))
-# print (accuracy_score(y_pred_SSVM,y_test_all.T))
+print ("Write completed, The result is now in data/Yte.csv.")
+# print (accuracy_score(y_pred_test2, y_test2))
